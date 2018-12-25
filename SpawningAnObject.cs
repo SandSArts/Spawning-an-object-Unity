fe0b85e79c6723c0831ffe479365bf38574c3684
SandS Arts <sandsarts.developer@gmail.com>
2018-12-25 06:50:12 -0800

    Add files via upload
    This script helps you to spawn an object.

---
+++ SpawningAnObject.cs
@@ -0,0 +1,19 @@
+﻿// Programmed by Shashank.S - creator of SandS Arts. Mail me at - sandsarts.developer@gmail.com
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningAnObject : MonoBehaviour {
+public GameObject playerobj;
+public Transform spawnpoint;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("w"))
+		Instantiate(playerobj,spawnpoint.position,spawnpoint.rotation);
+	}
+}
